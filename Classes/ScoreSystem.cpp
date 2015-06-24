01ed15498fcfd3aac51272ce985f915c676909c6
Less <less.lucifer@gmail.com>
2015-06-24 19:38:41 +0700

    + add missing files

---
+++ Classes/ScoreSystem.cpp
@@ -0,0 +1,36 @@
+﻿#include "ScoreSystem.h"
+
+USING_NS_CC;
+
+ScoreSystem::ScoreSystem()
+: score(0) {}
+
+int ScoreSystem::getScore()
+{
+	return score;
+}
+
+void ScoreSystem::increaseScore(int dscore)
+{
+	score += dscore;
+	// gọi hàm thông báo
+	if (OnScoreChanged)
+	{
+		OnScoreChanged(this);
+	}
+}
+
+void ScoreSystem::decreaseScore(int dscore)
+{
+	score -= dscore;
+	if (score < 0)
+	{
+		score = 0;
+	}
+
+	// gọi hàm thông báo
+	if (OnScoreChanged)
+	{
+		OnScoreChanged(this);
+	}
+}
---
+++ Classes/ScoreSystem.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "cocos2d.h"
+#include <functional>
+
+class ScoreSystem
+{
+private:
+	// biến để lưu trữ điểm số hiện tại
+	int score;
+
+public:
+	// hàm tạo
+	ScoreSystem();
+
+	// getter để lấy ra điểm số hiện tại
+	int getScore();
+
+	// hàm tăng điểm
+	void increaseScore(int dscore);
+	// hàm giảm điểm
+	void decreaseScore(int dscore);
+
+	// đây là biến lưu trữ một cấu trúc hàm
+	// trong C++, ta dùng nó để lưu trữ hàm
+	// cần gọi thông báo khi điểm số thay đổi
+	// đại loại, các bạn cứ tưởng tượng nó lưu
+	// con trỏ đến một hàm để sau này gọi lại
+	std::function<void(ScoreSystem *)> OnScoreChanged;
+};
